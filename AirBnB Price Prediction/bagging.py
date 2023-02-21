85c9bf1ff43c364843c527a028c359c849112a97
marie_bouvard <marie.bouvard@student-cs.fr>
2023-02-21 11:48:07 +0100

    Added choice of metrics for evaluation and function descriptions

--- AirBnB Price Prediction/bagging.py
+++ AirBnB Price Prediction/bagging.py
@@ -10,14 +10,35 @@ from sklearn.ensemble import RandomForestRegressor
 #                                    Decision Tree                                       #
 #----------------------------------------------------------------------------------------#
 
-def create_a_decision_tree(X, y, params=None):
+def create_a_decision_tree(X, y, params=None, metrics = None):
+    '''
+    This function creates and evaluate a decision tree. Optionally, we can perform a GridSearch to 
+    obtain the best parameters or given specific parameters for our tree.
+
+    Input: 
+        - X: our data
+        - y: the corresponding labels, here the price
+        - params: a dictionnary containing paramters to be passed to a decision tree.
+        - metrics: a list of metrics to be used for the evaluation of our model.
+
+    Output:
+        - A dictonnary containing the performance of our decision tree over different metrics 
+    '''
+
     if params == None:
+        # If no parameters are given create a default decision tree
         decision_tree = DecisionTreeRegressor()
+
     else:
+        # Creating a decision tree with a given parameter dictionnary
         decision_tree = DecisionTreeRegressor(**params)
 
     # Evaluating performance 
-    decision_tree_scores = eval(decision_tree, X, y)
+    if metrics == None:
+        decision_tree_scores = eval(decision_tree, X, y)
+    else: 
+        decision_tree_scores = eval(decision_tree, X, y, scores=metrics)
+
     return decision_tree_scores
 
 
@@ -25,12 +46,33 @@ def create_a_decision_tree(X, y, params=None):
 #                                      Random Forest                                     #
 #----------------------------------------------------------------------------------------#
 
-def create_a_random_forest(X, y, params=None):
+def create_a_random_forest(X, y, params=None, metrics=None):
+    '''
+    This function creates and evaluate a Random Forest. Optionally, we can perform a GridSearch to 
+    obtain the best parameters or given specific parameters for our forest.
+
+    Input: 
+        - X: our data
+        - y: the corresponding labels, here the price
+        - params: a dictionnary containing paramters to be passed to a random forest.
+        - metrics: a list of metrics to be used for the evaluation of our model.
+
+    Output:
+        - A dictonnary containing the performance of our random forest over different metrics 
+    '''
+
     if params == None:
+        # If no parameters are given create a default Random Forest
         random_forest = RandomForestRegressor()
+
     else:
+        # Creating a Random Forest with a given parameter dictionnary
         random_forest = RandomForestRegressor(**params)
 
     # Evaluating performance 
-    random_forest_scores = eval(random_forest, X, y)
+    if metrics==None:
+        random_forest_scores = eval(random_forest, X, y)
+    else:
+        random_forest_scores = eval(random_forest, X, y, scores=metrics)
+
     return random_forest_scores
