c9f6127966a794c04039884d111132d717f21daf
marie_bouvard <marie.bouvard@student-cs.fr>
2023-02-22 10:35:11 +0100

    Added Extremely Randomized Trees

--- AirBnB Price Prediction/bagging.py
+++ AirBnB Price Prediction/bagging.py
@@ -1,3 +1,7 @@
+'''
+A python file to create different types of bagging methods.
+Author: Marie Bouvard
+'''
 import numpy as np
 import pandas as pd
 from utils import splits, search_parameters, eval 
@@ -114,3 +118,114 @@ def create_a_random_forest(X, y, grid_search=False, params=None, metrics=None, v
 
     return random_forest, y_test, y_pred
 
+
+#----------------------------------------------------------------------------------------#
+#                           Extremely Randomized Random Forest                           #
+#----------------------------------------------------------------------------------------#
+
+def create_an_extremely_randomized_forest(X, y, grid_search=False, params=None, metrics=None, verbose=1):
+    '''
+    This function creates and evaluate an extremely randomized Random Forest. Optionally, we can 
+    perform a GridSearch to obtain the best parameters or given specific parameters for our forest.
+
+    Input: 
+        - X: our data
+        - y: the corresponding labels, here the price
+        - params: a dictionnary containing paramters to be passed to a random forest.
+        - metrics: a list of metrics to be used for the evaluation of our model.
+
+    Output:
+        - extra_random_forest: the created extremely random forest, 
+        - y_test: the labels associated to the test set
+        - y_pred: the predicted values on a test set
+    '''
+
+    X_train, y_train, X_test, y_test = splits(X, y)
+
+    if grid_search == True:
+        # Create a Random Forest, find best parameters with GridSearch Function, and evaluate
+        extra_rf_params = {'criterion': ['poisson', 'absolute_error', 'squared_error', 'friedman_mse'],
+            'n_estimators': [5,25,50,75,100,500], 
+            'min_samples_split':[2,5,10,25],
+            'min_samples_leaf':[2,5,10,25],
+            'max_depth':[2,5,10,25,50,75,100,150],
+            'oob_score': [True, False]
+            }
+
+        # Obtain best parameters from grid search function
+        extra_rf_best_params = search_parameters(ExtraTreesRegressor(), extra_rf_params, X_train, y_train, verbose=1)
+        random_forest_best = ExtraTreesRegressor(**extra_rf_best_params)
+        
+    elif params == None:
+        # If no parameters are given create a default Random Forest
+        extra_random_forest = ExtraTreesRegressor()
+
+    else:
+        # Creating a Random Forest with a given parameter dictionnary
+        extra_random_forest = ExtraTreesRegressor(**params)
+
+    # Evaluating performance 
+    if metrics==None:
+        extra_random_forest_scores = eval(extra_random_forest, X, y)
+    else:
+        extra_random_forest_scores = eval(extra_random_forest, X, y, scores=metrics)
+
+    y_pred = extra_random_forest.predict(X_test)
+
+    return extra_random_forest, y_test, y_pred
+
+
+#----------------------------------------------------------------------------------------#
+#                           Extremely Randomized Random Forest                           #
+#----------------------------------------------------------------------------------------#
+
+def create_an_extremely_randomized_forest(X, y, grid_search=False, params=None, metrics=None, verbose=1):
+    '''
+    This function creates and evaluate an extremely randomized Random Forest. Optionally, we can 
+    perform a GridSearch to obtain the best parameters or given specific parameters for our forest.
+
+    Input: 
+        - X: our data
+        - y: the corresponding labels, here the price
+        - params: a dictionnary containing paramters to be passed to a random forest.
+        - metrics: a list of metrics to be used for the evaluation of our model.
+
+    Output:
+        - extra_random_forest: the created extremely random forest, 
+        - y_test: the labels associated to the test set
+        - y_pred: the predicted values on a test set
+    '''
+
+    X_train, y_train, X_test, y_test = splits(X, y)
+
+    if grid_search == True:
+        # Create a Random Forest, find best parameters with GridSearch Function, and evaluate
+        extra_rf_params = {'criterion': ['poisson', 'absolute_error', 'squared_error', 'friedman_mse'],
+            'n_estimators': [5,25,50,75,100,500], 
+            'min_samples_split':[2,5,10,25],
+            'min_samples_leaf':[2,5,10,25],
+            'max_depth':[2,5,10,25,50,75,100,150],
+            'oob_score': [True, False]
+            }
+
+        # Obtain best parameters from grid search function
+        extra_rf_best_params = search_parameters(ExtraTreesRegressor(), extra_rf_params, X_train, y_train, verbose=1)
+        random_forest_best = ExtraTreesRegressor(**extra_rf_best_params)
+        
+    elif params == None:
+        # If no parameters are given create a default Random Forest
+        extra_random_forest = ExtraTreesRegressor()
+
+    else:
+        # Creating a Random Forest with a given parameter dictionnary
+        extra_random_forest = ExtraTreesRegressor(**params)
+
+    # Evaluating performance 
+    if metrics==None:
+        extra_random_forest_scores = eval(extra_random_forest, X, y)
+    else:
+        extra_random_forest_scores = eval(extra_random_forest, X, y, scores=metrics)
+
+    y_pred = extra_random_forest.predict(X_test)
+
+    return extra_random_forest, y_test, y_pred
