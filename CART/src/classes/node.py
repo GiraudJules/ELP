6000ed06fa3efe17eb883e53f4eec23ca7a85f1f
Nicolas Bourriez <nicolas.bourriez@gmail.com>
2023-03-01 15:54:36 +0100

    Modified self.gini_value -> self.risk_value

--- CART/src/classes/node.py
+++ CART/src/classes/node.py
@@ -7,6 +7,6 @@ class Node:
         self.splitting_point = value
         self.left_child = None
         self.right_child = None
-        self.gini_value = None
+        self.risk_value = None
         self.col_index = None
         self.is_leaf = is_leaf
