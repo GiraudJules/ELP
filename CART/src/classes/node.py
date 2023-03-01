d6091a1052a1d32ad92430dabb45fd8aeb90f091
Nicolas Bourriez <nicolas.bourriez@gmail.com>
2023-03-01 16:30:42 +0100

    Deleted non useful self.col_index attribute & modified docstring

--- CART/src/classes/node.py
+++ CART/src/classes/node.py
@@ -1,12 +1,15 @@
+# Standard library imports
+from typing import List
+
+
 class Node:
     """
     Class to build a Node.
     """
 
     def __init__(self, value, is_leaf=False):
-        self.splitting_point = value
-        self.left_child = None
-        self.right_child = None
-        self.risk_value = None
-        self.col_index = None
-        self.is_leaf = is_leaf
+        self.splitting_point: float = value
+        self.left_child: List[list, list] = None
+        self.right_child: List[list, list] = None
+        self.risk_value: int = None
+        self.is_leaf: bool = is_leaf
