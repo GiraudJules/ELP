34e1439c610bbddd1284bde48d2557f7c526f7e2
Jules Giraud <julesgiraud.grd@gmail.com>
2023-03-01 16:24:02 +0100

    Added gini_index and calculate_gini_index func

--- CART/src/classes/classification_tree.py
+++ CART/src/classes/classification_tree.py
@@ -3,17 +3,35 @@
 
 # Third party imports
 import numpy as np
+from base_tree import BaseTree
 
+class ClassificationTree(BaseTree):
+    def __init__(self) -> None:
+        super().__init__(self)
 
-class ClassificationTree():
-    def __init__(self, max_depth:int, min_sample_leaf:int, min_samples_split:int):
-    def fit(self, X_features:np.array, y_features: np.array):
-        pass
-    def create_nodes(self, X_features:np.array, y_features: np.array):
-        pass
-    def get_split(self, X_features:np.array, y_features: np.array, splitting_value_index: int, splitting_value: float):
-        pass
-    def build_tree(self, node: Node(), current_depth: int):
-        pass
-    def predict(self, X_test:np.array):
-        pass
+    def gini_index(self, y):
+        size = len(y)
+        instances = np.zeros(len(self.classes))
+
+        for row in y:
+            class_label = int(row[-1])
+            instances[class_label] += 1
+
+        if size > 0:
+            squared_counts = [(val/size)**2 for val in instances]
+            gini = 1 - np.sum(squared_counts)
+        else:
+            gini = 1
+
+        return gini
+
+    def calculate_gini_index(self, child):
+        group_size = [len(child['left']), len(child['right'])]
+        left_gini = self.gini_index(child['left'])
+        right_gini =  self.gini_index(child['right'])
+
+        weight_left = group_size[0] / np.sum(group_size)
+        weight_right = group_size[1] / np.sum(group_size)
+        weighted_gini = weight_left * left_gini + weight_right * right_gini
+
+        return weighted_gini
