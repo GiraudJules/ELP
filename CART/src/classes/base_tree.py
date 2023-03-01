ad6856f145476d2b1e05e99c4871868b06317769
Nicolas Bourriez <nicolas.bourriez@gmail.com>
2023-03-01 14:03:01 +0100

    Added risk_function attribute & modified several methods

--- CART/src/classes/base_tree.py
+++ CART/src/classes/base_tree.py
@@ -1,11 +1,13 @@
 # Standard library imports
 from abc import ABC, abstractmethod
-from typing import Union
+from typing import Union, Dict, List
 
 # Third party imports
 import numpy as np
 
-#Local applications imports
+# Local applications imports
+from node import Node
+
 
 class BaseTree(ABC):
     """
@@ -13,15 +15,17 @@ class BaseTree(ABC):
     To build a child of this class and inherit the methods, need to implement the methods.
     """
 
-    def __init__(self,min_sample_leaf,max_depth,min_sample_split):
+    def __init__(self, min_sample_leaf, max_depth, min_sample_split):
+        super().__init__()
         self.root = None
         self.min_sample_leaf = min_sample_leaf
         self.max_depth = max_depth
         self.min_sample_split = min_sample_split
         self.classes = None
+        self.risk_function = None
 
     @abstractmethod
-    def fit(self, X_features:np.array, y_features: np.array) -> self.root:
+    def fit(self, X_features: np.array, y_features: np.array) -> Node():
         """
         - Retrieves the different classes from X_features and stores it into self.classes
         - Assign new value to self.node with self.create_node
@@ -40,7 +44,9 @@ class BaseTree(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def predict(self, X_test:Union[Union[int,str],np.array]) -> Union[Union[int, str], np.array]:
+    def predict(
+        self, X_test: Union[Union[int, str], np.array]
+    ) -> Union[Union[int, str], np.array]:
         """
         Predict class for whether:
         - Regression: a single int OR multiple int
@@ -56,17 +62,61 @@ class BaseTree(ABC):
             Union[Union[int, str], np.array]: whether a single int or str; or np.array
 
         """
-      
+
         raise NotImplementedError
 
     @abstractmethod
     def build_tree(self, node: Node(), current_depth: int) -> None:
-        ""
-    
+        """
+        Build the tree recursively.
+
+        Args:
+            node (Node()): current node
+            current_depth (int): current depth of the tree
+
+        Raises:
+            NotImplementedError: if the method is not implement
+        """
         raise NotImplementedError
-        
+
     @abstractmethod
-    def split_dataset(self,)
-        
+    def split_dataset(
+        self,
+        X_features: np.array,
+        y_features: np.array,
+        splitting_point_index: int,
+        splitting_point: float,
+    ) -> Dict[str("left") : List, str("right") : List]:
+        """
+        Split dataset into left and right datasets.
+
+        Args:
+            X_features (np.array): X features from dataset
+            y_features (np.array): y features from dataset
+            splitting_point_index (int): index of the splitting point
+            splitting_point (float): value of the splitting point
+
+        Raises:
+            NotImplementedError: if the method is not implement
+
+        Returns:
+            Dict[str('left'):List, str('right'):List]: dictionary with left and right datasets
+        """
+        raise NotImplementedError
+
     @abstractmethod
-    def 
+    def create_node(self, X_features: np.array, y_features: np.array) -> Node():
+        """
+        Create a new node
+
+        Args:
+            X_features (np.array): X features from dataset
+            y_features (np.array): y features from dataset
+
+        Raises:
+            NotImplementedError: if the method is not implement
+
+        Returns:
+            Node(): new node
+        """
+        raise NotImplementedError
